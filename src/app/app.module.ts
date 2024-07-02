81d593b364e78453845c464d5561d74064623f46
YHors Brayan <rs.brayan26@gmail.com>
2024-07-01 21:50:39 -0500

    ajuste hora pais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
-import { NgModule } from "@angular/core";
+import { LOCALE_ID, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { registerLocaleData } from "@angular/common";
 
 import { AppComponent } from "./app.component";
 import { PasswordPipe } from "./pipes/password.pipe";
 import { DomSecurePipe } from "./pipes/dom-secure.pipe";
 import { CapitalizedPipe } from "./pipes/capitalized.pipe";
 
+import localEs from "@angular/common/locales/es";
+import localFr from "@angular/common/locales/fr";
+
+registerLocaleData(localEs);
+registerLocaleData(localFr);
+
 @NgModule({
   declarations: [AppComponent, PasswordPipe, DomSecurePipe, CapitalizedPipe],
   imports: [BrowserModule],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "es",
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
